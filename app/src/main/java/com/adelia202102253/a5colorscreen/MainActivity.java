d5fb9f959488d046a3c42da07f1584b84c065636
adeliarizki <adeliarizki89@gmail.com>
2023-03-15 11:43:35 +0700

    Initial commit

---
+++ app/src/main/java/com/adelia202102253/a5colorscreen/MainActivity.java
@@ -0,0 +1,13 @@
+package com.adelia202102253.a5colorscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
